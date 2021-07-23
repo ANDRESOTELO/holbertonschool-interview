30e76b5342259e0f3ce6c135fb2015b007ff7687
ANDRESOTELO <andresotelo2020@gmail.com>
2021-07-22 23:59:10 -0500

    ADDED | Lockboxes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -6,12 +6,11 @@ each box may contain keys to the other boxes.
 '''
 
 
-
 def canUnlockAll(boxes):
     """Method that determines if all the boxes can be opened"""
 
     total_boxes = len(boxes)
-    lista_boxes = list(range(1, total_boxes))
+    lista_boxes = list(range(0, total_boxes))
     keys_list = boxes[0]
     
     if total_boxes <= 1:
@@ -29,3 +28,7 @@ def canUnlockAll(boxes):
         return True
     else:
         return False
+
+boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+print(canUnlockAll(boxes))
+
