9223a95fbdbebd7162e2e40e53b211d9f2396c0c
ANDRESOTELO <andresotelo2020@gmail.com>
2021-07-22 23:57:12 -0500

    ADDED | Lockboxes

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+'''
+You have n number of locked boxes in front of you. 
+Each box is numbered sequentially from 0 to n - 1 and 
+each box may contain keys to the other boxes.
+'''
+
+
+
+def canUnlockAll(boxes):
+    """Method that determines if all the boxes can be opened"""
+
+    total_boxes = len(boxes)
+    lista_boxes = list(range(1, total_boxes))
+    keys_list = boxes[0]
+    
+    if total_boxes <= 1:
+        return True
+    
+    for key in keys_list:
+        if key in lista_boxes:
+            for key_box in boxes[key]:
+                if key_box not in keys_list:
+                    keys_list.append(key_box)
+    
+    keys_list.sort()
+
+    if keys_list == lista_boxes:
+        return True
+    else:
+        return False
