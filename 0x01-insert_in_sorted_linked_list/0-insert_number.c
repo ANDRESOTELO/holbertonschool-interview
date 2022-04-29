c19f9a65add32cb5a97449cf49a7aabde637a1e5
ANDRESOTELO <andresotelo2020@gmail.com>
2022-04-28 21:04:07 -0500

    ADDED | FIXED Linked List

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -11,32 +11,40 @@
 
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new_node = NULL, *current = NULL;
+	listint_t *new_node = NULL;
+	listint_t *current_node = NULL;
 
 	if (head == NULL)
-			return (NULL);
-
-	current = *head;
-
+	{
+		return (NULL);
+	}
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
-		return (NULL);
-	new_node->n = number;
-
-	if (current != NULL)
 	{
-		while (current->next != NULL)
+		return (NULL);
+	}
+	current_node = *head;
+	new_node->n = number;
+	if (current_node)
+	{
+		if (current_node->n > number)
 		{
-			if (number < current->next->n)
+			new_node->next = prev_node;
+			*head = new_node;
+			return (new_node);
+		}
+		while (current_node->next)
+		{
+			if (current_node->next->n > number)
 			{
-				new_node->next = current->next;
-				current->next = new_node;
+				new_node->next = current_node->next;
+				current_node->next = new_node;
 				return (new_node);
 			}
-			current = current->next;
+			current_node = current_node->next;
 		}
 		new_node->next = NULL;
-		current->next = new_node;
+		current_node->next = new_node;
 		return (new_node);
 	}
 	new_node->next = NULL;
