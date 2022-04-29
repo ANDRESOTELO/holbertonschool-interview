8b165d65fa9261824f6ad353fb642db4aecadfb5
ANDRESOTELO <andresotelo2020@gmail.com>
2022-04-28 20:37:17 -0500

    ADDED | FIXED Linked List

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -1,59 +1,48 @@
 #include "lists.h"
 
 /**
- *add_node - Function to insert a node
+ *insert_node - Function to insert a node
  *@head: pointer to next node
  *@number: number to add to new node
- *Return: The number of nodes
+ *Return: new node
  */
 
 listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *new_node = NULL;
-    listint_t *current_node = NULL;
+	listint_t *current_node = NULL;
 
-    /* If head doesn´t exists */
-    if (!head)
-        return (NULL);
-
-    /* Allocate memory to new node */
+	if (!head)
+		return (NULL);
 	new_node = malloc(sizeof(listint_t));
-
-    /* Check if new node allocate memory success */
 	if (!new_node)
 		return (NULL);
-
-    /* Assign head to current node */
-    current_node = *head;
-
-    /* Assign number to the variable in the new node */
+	current_node = *head;
 	new_node->n = number;
 
 	if (current_node)
 	{
-        if (current_node->n > number)
-        {
-            new_node->next = prev_node;
+		if (current_node->n > number)
+		{
+			new_node->next = prev_node;
 			*head = new_node;
 			return (new_node);
-        }
-
-        while (current_node->next)
-        {
-            if (current_node->next->n > number)
-            {
-                new_node->next = current_node->next;
-                current_node->next = new_node;
-                return (new_node);
-            }
-            current_node = current_node->next;
-        }
-
-        new_node->next = NULL;
-        current_node->next = new_node;
-        return (new_node);
-    }
-    new_node->next = NULL;
-    *head = new_node;
-    return (new_node);
+		}
+		while (current_node->next)
+		{
+			if (current_node->next->n > number)
+			{
+				new_node->next = current_node->next;
+				current_node->next = new_node;
+				return (new_node);
+			}
+			current_node = current_node->next;
+		}
+		new_node->next = NULL;
+		current_node->next = new_node;
+		return (new_node);
+	}
+	new_node->next = NULL;
+	*head = new_node;
+	return (new_node);
 }
