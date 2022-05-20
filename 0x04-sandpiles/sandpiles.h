2e1f4394692771a4c612c508de6e6405f1267550
ANDRESOTELO <andresotelo2020@gmail.com>
2022-05-19 20:01:37 -0500

    ADDED | Sandpiles

---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,14 @@
+#ifndef SANDPILES_H
+#define SANDPILES_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+void sandpiles(int grid1[3][3], int grid2[3][3]);
+
+void copy(int grid_result[3][3], int grid_sum[3][3]);
+
+void toppling(int grid1[3][3]);
+
+#endif /* SANDPILES_H */
