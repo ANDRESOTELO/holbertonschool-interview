1991662abf5a028496bd061add49546b5b37238f
ANDRESOTELO <andresotelo2020@gmail.com>
2022-10-13 17:52:20 -0500

    ADDED | Sort

---
+++ 0x17-find_the_loop/0-find_loop.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * find_listint_loop - Find the memory address
+ * of starting node
+ * @head: Pointer to the head
+ * Return: The address of the node or NULL
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *ab, *cd;
+
+	if (!head)
+		return (NULL);
+
+	ab = cd = head;
+
+	while (cd && ab && ab->next)
+	{
+		cd = cd->next;
+		ab = (ab->next)->next;
+
+		if (ab == cd)
+		{
+			cd = head;
+			while (cd && ab)
+			{
+				if (cd == ab)
+					return (cd);
+
+				cd = cd->next;
+				ab = ab->next;
+			}
+		}
+	}
+	return (NULL);
+}
