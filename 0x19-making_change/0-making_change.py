2a530f89a2a52a73c21e712c8b50711f855bc015
ANDRESOTELO <andresotelo2020@gmail.com>
2022-10-27 09:35:28 -0500

    ADDED | Making Change

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
-"""Make change script"""
+"""Make changes script"""
 
 
 def makeChange(coins, total):
-    """Make change function"""
-    change = 0
+    """Make changes function"""
+    changes = 0
     sw = True
 
     if (total <= 0):
         return 0
     else:
         while sw is True:
-            save = change
+            save = changes
             for coin in sorted(coins, reverse=True):
                 if total >= coin:
                     if total % coin == 0:
-                        change = int(change + total / coin)
+                        changes = int(changes + total / coin)
                         sw = False
                         break
                     else:
-                        change = int(change + total / coin)
+                        changes = int(changes + total / coin)
                         total = total % coin
                         break
-            if change is save:
+            if changes is save:
                 return -1
-    return int(change)
+    return int(changes)
